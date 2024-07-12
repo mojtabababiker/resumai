bb0c571bf1ba1ac72563aa3de4c50d0d83d73977
mojtabababiker <mojmohammad98@gmail.com>
2024-07-12 09:45:10 +0200

    Update the api_v1 routes and schemas, add get user profile endpoint and its schema

--- backend/app/v1/schema/user_schemas.py
+++ backend/app/v1/schema/user_schemas.py
@@ -3,9 +3,42 @@
 from pydantic import BaseModel, EmailStr
 
 
-class UserCreate(BaseModel):
-    """The user creation schema"""
+class UserBase(BaseModel):
+    """The user base schema
+    This schema is used to define the common fields for the user schema
+
+    * first_name: str: the user's first name
+    * last_name: str: the user's last name
+    * email: EmailStr: the user's email address
+    """
     first_name: str
     last_name: str
     email: EmailStr
-    password: str  # type: ignore
+
+
+class UserCreate(UserBase):
+    """The user creation schema
+    This schema is used to define the fields required to create a user
+    * first_name: str: the user's first name
+    * last_name: str: the user's last name
+    * email: EmailStr: the user's email address
+    * password: str: the user's password
+    """
+    password: str
+
+class UserOut(UserBase):
+    """The user output schema
+    This schema is used to define the fields to be returned in the response
+    
+    * id: str: the user's id
+    * first_name: str: the user's first name
+    * last_name: str: the user's last name
+    * email: EmailStr: the user's email address
+    * created_at: str: the user's creation date
+    * updated_at: str: the user's last update date
+    * is_active: bool: the user's active status
+    """
+    id: str
+    created_at: str
+    updated_at: str
+    is_active: bool
--- backend/app/v1/views/users.py
+++ backend/app/v1/views/users.py
@@ -4,11 +4,11 @@ from typing import Annotated
 from fastapi import APIRouter, Body, Depends, HTTPException, status
 
 # import schemas
-from app.v1.schema.user_schemas import UserCreate
+from app.v1.schema.user_schemas import UserCreate, UserOut
 from app.v1.schema.auth_schemas import Token
 
 # import dependencies
-from app.v1.utils.access_token import create_access_token
+from app.v1.utils.access_token import create_access_token, get_current_user
 
 # User database model
 from models.user import User
@@ -40,3 +40,24 @@ async def create_user(user: Annotated[UserCreate, Body()]) -> Token:
     await new_user.save()
     access_token = await create_access_token(new_user.id)
     return Token(access_token=access_token, token_type="bearer")
+
+@router.get('/me')
+async def get_me(user: User = Depends(get_current_user)) -> UserOut:
+    """Get the current user's data
+    
+    Parameters:
+    -----------
+    * **user**: User: the current user
+    
+    Returns: UserOut: the current user's data
+    """
+    user_profile = {
+        "id": user.id,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+        "created_at": user.created_at,
+        "updated_at": user.updated_at,
+        "is_active": user.is_active
+    }
+    return UserOut(**user_profile)
